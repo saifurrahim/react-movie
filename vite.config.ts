fe897d88363245b7aa88abbc5e276f67c54ece9a
unknown <saifurrahimwp@gmail.com>
2025-04-07 10:10:48 +0700

    Remove .env file from repository

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, './');
+
+  return {
+    plugins: [react()],
+    envDir: './',
+    server: {
+      proxy: {
+        '/tmdb-api': {
+          target: env.VITE_TMDB_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/tmdb-api/, ''),
+          headers: {
+            'Authorization': `Bearer ${env.VITE_TMDB_API_ACCESS_TOKEN}`,
+            'Accept' : 'application/json'
+          },
+        },
+      },
+    },
+  }
 })
