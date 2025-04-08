f10df92f3a0e4ccb1567f6326359dccae0b1d1fa
unknown <saifurrahimwp@gmail.com>
2025-04-08 21:26:51 +0700

    Back to basic : templating the UI for the base

--- src/NowPlaying.tsx
+++ src/NowPlaying.tsx
@@ -10,47 +10,50 @@ function NowPlaying()
 
     const movieContext = useContext(context);
 
-    // onMounted
+
+    // onMounted  
     useEffect(() => {
         const fetchMovies = async () => {
-
-            const nowPlayingList = await movieService.getNowPLayingList();
-
-            setMovies(nowPlayingList);
+            const nowPlayingData = await movieService.getNowPLayingList();
+            setMovies(nowPlayingData);
         }
-
         fetchMovies();
-
-        const list = document.getElementById('nowPlaying');
-
-        const handleWheel = (event: WheelEvent) => {
-            event.preventDefault();
-            (list as HTMLElement).scrollLeft += event.deltaY;
-          };
-      
-          list?.addEventListener('wheel', handleWheel);
-      
-          return () => {
-            list?.removeEventListener('wheel', handleWheel);
-          };
     });
 
     return (
         <>
-        <h2 className="text-2xl font-semibold mb-2">Now Playing</h2>
-        <ul id="nowPlaying" className="flex w-full overflow-x-auto overflow-y-hidden gap-2">
-            {movies.map((movie : any) => (
-                <li key={movie.id} className="relative basis-auto shrink-0 flex flex-col items-center">
-                    <div className="inline-block h-[360px]">
-                        <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
-                    </div>
-                    <div className="absolute bottom-0 left-0 w-full h-[20%] flex flex-col items-center p-2 bg-[rgba(0,0,0,.5)]">
-                        <b className="text-white">{movie.original_title}</b>
-                        <small className="text-white">{movieContext.getGenreNames(movie.genre_ids)}</small>
-                    </div>
-                </li>
-            ))}
-        </ul>
+        <div id="nowPlaying" className="bg-indigo-500 h-[500px] grid md:grid-cols-3">
+
+            <div className="bg-green-300 hidden md:block"></div>
+            <div className="col-span-2 bg-orange-300 relative">
+                <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit possimus animi, repudiandae, fugiat cum iste aliquam similique atque culpa excepturi repellat nam aliquid tenetur sit eos. Dignissimos velit veniam sint.</span>
+
+                <ul className="absolute bottom-0 right-0 w-full bg-pink-500 h-[150px]"></ul>
+            </div>
+{/* 
+            <div className="w-full h-full flex flex-col">
+
+                <h2 className="text-2xl font-semibold mb-2" style={{background: 'linear-gradient(to top, white, gold)', WebkitBackgroundClip: 'text', WebkitTextFillColor: "transparent"}}>Now Playing</h2>
+
+                <div className="h-[480px] flex justify-center">
+                    <img src="https://image.tmdb.org/t/p/original/yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg" alt="" className="w-[320px]"/>
+                </div>
+
+
+            </div>
+
+            <div className="absolute bottom-0 right-0  bg-[rgba(0,0,0,.75)] p-2 rounded shadow-md">
+                <ul className="flex max-w-[720px] overflow-x-auto overflow-y-hidden gap-2">
+                    {movies.map((movie : any) => {
+                        return <li key={movie.id} className="relative basis-auto shrink-0 flex flex-col items-center">
+                            <div className="inline-block h-[180px]">
+                                <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
+                            </div>
+                        </li>;
+                    })}
+                </ul>
+            </div> */}
+        </div>
         </>
     )
 } 
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import NowPlaying from './NowPlaying'
 
 createRoot(document.getElementById('root')!).render(
     <MovieContainer>
+      <div className='grid grid-rows-3'>
       <NowPlaying />
+      <div className='bg-red-300'></div>
+      <div className='bg-blue-300'></div>
+      </div>
     </MovieContainer>,
 )
