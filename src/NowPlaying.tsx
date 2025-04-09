f1fe3a8ec85d369095540f1ad42f6152a9d0e975
unknown <saifurrahimwp@gmail.com>
2025-04-09 11:23:19 +0700

    Adding Movie models and now playing title and overview on list selection

--- src/NowPlaying.tsx
+++ src/NowPlaying.tsx
@@ -1,42 +1,65 @@
 import { useEffect, useState, useContext } from "react";
 import movieService from "./services/movie.service"
 import { context } from "./MovieApp";
+import Movie from "./models/Movie";
+import './NowPlaying.css';
 
 
 function NowPlaying()
 {
 
     const [movies, setMovies] = useState([]);
+    const [movie, setMovie] = useState<Movie | null>(null);
 
     const movieContext = useContext(context);
 
+    const listClick = (e : React.MouseEvent<HTMLElement>, listIndex : number) => {
+        document.querySelector('#nowPlaying ul li.active')?.classList.remove('active');
 
-    // onMounted  
+        let movie = movies[listIndex] as Movie;
+
+        setMovie(movie);
+
+        e.currentTarget.classList.add('active');
+    };
+
+    // Run after render  
     useEffect(() => {
         const fetchMovies = async () => {
             const nowPlayingData = await movieService.getNowPLayingList();
             setMovies(nowPlayingData);
+            setMovie(nowPlayingData[0]);
         }
         fetchMovies();
-    });
+    },[]);
+    // the second parameter is the dependency, 
+    // if undefined : the function always retriggerring every time react component rerendered.
+    // if [] : the function only run on the first render (the first time the component mounted/ready)
+    // if [movies, backdrop.. etc] : the function will rerun if the defined state rerendered
 
     return (
         <>
         <div id="nowPlaying" className="bg-indigo-500 h-[500px] flex">
 
-            <div className="bg-green-300 hidden md:block basis-auto shrink-0 h-[500px]">
-                
-            <img src="https://image.tmdb.org/t/p/original//yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg" alt="" className="h-full"/>
+            <div className="bg-green-300 hidden md:block basis-auto shrink-0 h-[500px] relative">
+                <img key="poster" src={((movie?.poster_path ?? "") == "" ? undefined : movieService.getImagePath(movie?.poster_path ?? ""))} alt="" className="h-full"/>
+                <div className="absolute bottom-0 right-0 bg-[rgba(255,0,0,.25)] p-2">
+                    <span className="text-white font-bold text-2xl">Now Playing</span>
+                </div>
             </div>
-            <div className="bg-orange-300 relative bg-[url(https://image.tmdb.org/t/p/original/2Nti3gYAX513wvhp8IiLL6ZDyOm.jpg)] bg-cover">
-                <div className="h-full w-full bg-[rgba(0,0,0,.25)] text-white">
-                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In omnis facere eligendi laboriosam, distinctio eum deserunt dolor corporis voluptatem cupiditate, ratione nemo aliquam quis dignissimos modi ullam tempora explicabo officia?</span>
+
+            <div key="backdrop" className="bg-orange-300 relative" style={{backgroundImage: `url(${movieService.getImagePath(movie?.backdrop_path ?? "")})`, backgroundSize: 'cover'}}>
+                <div className="h-full w-full bg-[rgba(0,0,0,.25)] text-white p-4">
+                    <h2 key="title" className="font-bold text-2xl">{movie?.original_title}</h2>
+                    <span key="overview">
+                        {movie?.overview}
+                    </span>
                 </div>
 
                 <div className="absolute bottom-0 right-0 w-full h-[150px] rounded bg-[rgba(0,0,0,.5)] p-2 shadow-md">
                     <ul className="flex gap-2 overflow-x-auto overflow-y-hidden h-full w-full">
                         {movies.map((movie: any, index : number) => {
-                            return <li key={index} className="basis-auto shrink-0">
+                            return <li key={index} className={`shrink-0 cursor-pointer ${index === 0 ? 'active' : ''}`} onClick={(event) => listClick(event, index)}>
                                 <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
                             </li>
                         })}
@@ -44,29 +67,6 @@ function NowPlaying()
                 </div>
 
             </div>
-{/* 
-            <div className="w-full h-full flex flex-col">
-
-                <h2 className="text-2xl font-semibold mb-2" style={{background: 'linear-gradient(to top, white, gold)', WebkitBackgroundClip: 'text', WebkitTextFillColor: "transparent"}}>Now Playing</h2>
-
-                <div className="h-[480px] flex justify-center">
-                    <img src="https://image.tmdb.org/t/p/original/yFHHfHcUgGAxziP1C3lLt0q2T4s.jpg" alt="" className="w-[320px]"/>
-                </div>
-
-
-            </div>
-
-            <div className="absolute bottom-0 right-0  bg-[rgba(0,0,0,.75)] p-2 rounded shadow-md">
-                <ul className="flex max-w-[720px] overflow-x-auto overflow-y-hidden gap-2">
-                    {movies.map((movie : any) => {
-                        return <li key={movie.id} className="relative basis-auto shrink-0 flex flex-col items-center">
-                            <div className="inline-block h-[180px]">
-                                <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
-                            </div>
-                        </li>;
-                    })}
-                </ul>
-            </div> */}
         </div>
         </>
     )
---
+++ src/models/Movie.ts
@@ -0,0 +1,66 @@
+class Movie {
+    adult : boolean;
+    backdrop_path : string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+
+    constructor({
+        adult,
+        backdrop_path,
+        genre_ids,
+        id,
+        original_language,
+        original_title,
+        overview,
+        popularity,
+        poster_path,
+        release_date,
+        title,
+        video,
+        vote_average,
+        vote_count,
+      }: {
+        adult: boolean;
+        backdrop_path: string;
+        genre_ids: number[];
+        id: number;
+        original_language: string;
+        original_title: string;
+        overview: string;
+        popularity: number;
+        poster_path: string;
+        release_date: string;
+        title: string;
+        video: boolean;
+        vote_average: number;
+        vote_count: number;
+      }) 
+      {
+        this.adult = adult;
+        this.backdrop_path = backdrop_path;
+        this.genre_ids = genre_ids;
+        this.id = id;
+        this.original_language = original_language;
+        this.original_title = original_title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.poster_path = poster_path;
+        this.release_date = release_date;
+        this.title = title;
+        this.video = video;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+      }
+}
+
+export default Movie
