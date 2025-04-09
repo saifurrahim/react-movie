9b8fed38d66a0ff3cd2db98bc59669266fbbba15
unknown <saifurrahimwp@gmail.com>
2025-04-09 22:56:05 +0700

    Adding loading when component not ready

--- src/NowPlaying.tsx
+++ src/NowPlaying.tsx
@@ -7,7 +7,7 @@ import './NowPlaying.css';
 
 function NowPlaying()
 {
-
+    const [isReady, setReady] = useState(false);
     const [movies, setMovies] = useState([]);
     const [movie, setMovie] = useState<Movie | null>(null);
     const [isLoading, setLoading] = useState(false);
@@ -37,6 +37,10 @@ function NowPlaying()
             setMovie(nowPlayingData[0]);
         }
         fetchMovies();
+
+        setTimeout(() => {
+            setReady(true);
+        },2000);
     },[]);
     // the second parameter is the dependency, 
     // if undefined : the function always retriggerring every time react component rerendered.
@@ -45,42 +49,50 @@ function NowPlaying()
 
     return (
         <>
-        <div id="nowPlaying" className="bg-indigo-500 h-[500px] flex">
+        <div id="nowPlaying" className="bg-indigo-500 h-[500px]">
+        {isReady ? 
+        (
+            <div className="flex h-full">
+                <div className="bg-gray-800 hidden md:block basis-auto shrink-0 h-[500px] relative">
+                    <img key="poster" src={((movie?.poster_path ?? "") == "" ? undefined : movieService.getImagePath(movie?.poster_path ?? ""))} alt="Poster" className="h-full transition-opacity duration-500" style={{opacity: isLoading ? .25 : 1}}/>
+                    <div className="absolute bottom-0 right-0 bg-[rgba(255,0,0,.25)] p-2">
+                        <span className="text-white font-bold text-2xl">Now Playing</span>
+                    </div>
+                </div>
 
-            <div className="bg-green-300 hidden md:block basis-auto shrink-0 h-[500px] relative">
-                <img key="poster" src={((movie?.poster_path ?? "") == "" ? undefined : movieService.getImagePath(movie?.poster_path ?? ""))} alt="Poster" className="h-full transition-opacity duration-500" style={{opacity: isLoading ? 0 : 1}}/>
-                <div className="absolute bottom-0 right-0 bg-[rgba(255,0,0,.25)] p-2">
-                    <span className="text-white font-bold text-2xl">Now Playing</span>
+                <div className="bg-gray-300 relative" >
+                    <div key="backdrop" className="h-full w-full text-white p-4 flex flex-col transition-opacity duration-500" style={{backgroundImage: `url(${movieService.getImagePath(movie?.backdrop_path ?? "")})`, backgroundSize: 'cover', opacity: isLoading ? .25 : 1}}>
+                        <article className="bg-[rgba(0,0,0,.5)] p-2 rounded">
+                            <h2 key="title" className="font-bold text-2xl">{movie?.original_title}</h2>
+                            
+                            <div className="my-2">
+                                <span key="voteAverage" className="bg-white rounded-full px-3 py-1 text-black shadow-md">Score : <b>{movie?.vote_average.toFixed(2)}</b> / 10.00</span>
+                                <small key="voteCount" className="font-semibold"> from {movie?.vote_count} votes</small>
+                            </div>
+
+                            <p key="overview">
+                                {movie?.overview}
+                            </p>
+                        </article>
+                    </div>
+
+                    <div className="absolute bottom-0 right-0 w-full h-[150px] rounded bg-[rgba(0,0,0,.5)] p-2 shadow-md">
+                        <ul className="flex gap-2 overflow-x-auto overflow-y-hidden h-full w-full">
+                            {movies.map((movie: any, index : number) => {
+                                return <li key={index} className={`shrink-0 cursor-pointer ${index === 0 ? 'active' : ''}`} onClick={(event) => listClick(event, index)}>
+                                    <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
+                                </li>
+                            })}
+                        </ul>
+                    </div>
                 </div>
             </div>
-
-            <div className="bg-orange-300 relative" >
-                <div key="backdrop" className="h-full w-full text-white p-4 flex flex-col transition-opacity duration-500" style={{backgroundImage: `url(${movieService.getImagePath(movie?.backdrop_path ?? "")})`, backgroundSize: 'cover', opacity: isLoading ? 0 : 1}}>
-                    <article className="bg-[rgba(0,0,0,.5)] p-2 rounded">
-                        <h2 key="title" className="font-bold text-2xl">{movie?.original_title}</h2>
-                        
-                        <div className="my-2">
-                            <span key="voteAverage" className="bg-white rounded-full px-3 py-1 text-black shadow-md">Score : <b>{movie?.vote_average.toFixed(2)}</b> / 10.00</span>
-                            <small key="voteCount" className="font-semibold"> from {movie?.vote_count} votes</small>
-                        </div>
-
-                        <p key="overview">
-                            {movie?.overview}
-                        </p>
-                    </article>
-                </div>
-
-                <div className="absolute bottom-0 right-0 w-full h-[150px] rounded bg-[rgba(0,0,0,.5)] p-2 shadow-md">
-                    <ul className="flex gap-2 overflow-x-auto overflow-y-hidden h-full w-full">
-                        {movies.map((movie: any, index : number) => {
-                            return <li key={index} className={`shrink-0 cursor-pointer ${index === 0 ? 'active' : ''}`} onClick={(event) => listClick(event, index)}>
-                                <img src={movieService.getImagePath(movie.poster_path)} alt={movie.original_title} className="h-full"/>
-                            </li>
-                        })}
-                    </ul>
-                </div>
-
+        ) : (
+            <div className="animate-pulse h-full w-full bg-emerald-500 flex justify-center items-center">
+                <span className="animate-bounce">LOADING...</span>
             </div>
+        )}    
+
         </div>
         </>
     )
