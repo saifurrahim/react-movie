49a573c93e119e6ab3b623a6f494d81bd0111a54
unknown <saifurrahimwp@gmail.com>
2025-04-10 11:00:28 +0700

    Add clapper loading animation

--- src/NowPlaying.tsx
+++ src/NowPlaying.tsx
@@ -10,20 +10,20 @@ function NowPlaying()
     const [isReady, setReady] = useState(false);
     const [movies, setMovies] = useState([]);
     const [movie, setMovie] = useState<Movie | null>(null);
-    const [isLoading, setLoading] = useState(false);
+    const [isTransitioning, setTransitioning] = useState(false);
 
     const movieContext = useContext(context);
 
     const listClick = (e : React.MouseEvent<HTMLElement>, listIndex : number) => {
         document.querySelector('#nowPlaying ul li.active')?.classList.remove('active');
 
-        setLoading(true);
+        setTransitioning(true);
 
         let movie = movies[listIndex] as Movie;
 
         setTimeout(() => {
             setMovie(movie);
-            setLoading(false);
+            setTransitioning(false);
         }, 500);
 
         e.currentTarget.classList.add('active');
@@ -54,14 +54,14 @@ function NowPlaying()
         (
             <div className="flex h-full">
                 <div className="bg-gray-800 hidden md:block basis-auto shrink-0 h-[500px] relative">
-                    <img key="poster" src={((movie?.poster_path ?? "") == "" ? undefined : movieService.getImagePath(movie?.poster_path ?? ""))} alt="Poster" className="h-full transition-opacity duration-500" style={{opacity: isLoading ? .25 : 1}}/>
+                    <img key="poster" src={((movie?.poster_path ?? "") == "" ? undefined : movieService.getImagePath(movie?.poster_path ?? ""))} alt="Poster" className="h-full transition-opacity duration-500" style={{opacity: isTransitioning ? .25 : 1}}/>
                     <div className="absolute bottom-0 right-0 bg-[rgba(255,0,0,.25)] p-2">
                         <span className="text-white font-bold text-2xl">Now Playing</span>
                     </div>
                 </div>
 
                 <div className="bg-gray-300 relative" >
-                    <div key="backdrop" className="h-full w-full text-white p-4 flex flex-col transition-opacity duration-500" style={{backgroundImage: `url(${movieService.getImagePath(movie?.backdrop_path ?? "")})`, backgroundSize: 'cover', opacity: isLoading ? .25 : 1}}>
+                    <div key="backdrop" className="h-full w-full text-white p-4 flex flex-col transition-opacity duration-500" style={{backgroundImage: `url(${movieService.getImagePath(movie?.backdrop_path ?? "")})`, backgroundSize: 'cover', opacity: isTransitioning ? .25 : 1}}>
                         <article className="bg-[rgba(0,0,0,.5)] p-2 rounded">
                             <h2 key="title" className="font-bold text-2xl">{movie?.original_title}</h2>
                             
@@ -88,8 +88,10 @@ function NowPlaying()
                 </div>
             </div>
         ) : (
-            <div className="animate-pulse h-full w-full bg-emerald-500 flex justify-center items-center">
-                <span className="animate-bounce">LOADING...</span>
+            <div className="h-full w-full bg-white-500 flex justify-center items-center">
+                <div className="clapper">
+                    Test
+                </div>
             </div>
         )}    
 
